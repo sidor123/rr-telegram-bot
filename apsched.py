a1682351f7c63b0c7a22f313666f76bb1d9ec00b
sidor123 <74910028+sidor123@users.noreply.github.com>
2023-08-13 12:50:23 +0500

    Update apsched.py

--- apsched.py
+++ apsched.py
@@ -6,6 +6,7 @@ from main import bot, lessons, students
 async def send_message_to_students(*data):
     info = await lessons.check_a_by_b('*', 'group_id', data[0])
     for row in await students.check_a_by_b('*', 'group_id', data[0]):
+        print(info)
         inline_kb = InlineKeyboardMarkup().add(InlineKeyboardButton(text="✔", callback_data=str(info[0])),
                                                InlineKeyboardButton(text="❌", callback_data="no" + str(info[0])))
         send_message_text = f"📌 <b>{row[1]} {row[2]}</b>, предупреждаем, через 24 часа ({info[1]}.{info[2]}.{info[3]}" \
