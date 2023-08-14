3f213723ce618cefd9d011985a2b6d5716b4de0f
sidor123 <74910028+sidor123@users.noreply.github.com>
2023-08-14 16:18:06 +0500

    Update sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -72,8 +72,9 @@ class Lessons:
         return cur.fetchall()
 
     async def set_students_presence(self, presence, student_id, lesson_id):
-        query = cur.execute("SELECT * FROM lessons")
-        print(query.fetchall())
+        cur.execute("SELECT * FROM lessons")
+        record = cur.fetchall()
+        print(record)
         present_record = cur.execute("SELECT present_students FROM lessons where id = '%s'", (lesson_id, )).fetchall()[0]
         non_present_record = cur.execute("SELECT not_present_students FROM lessons where id = '%s'", (lesson_id, )).fetchall()[0]
         if presence:
