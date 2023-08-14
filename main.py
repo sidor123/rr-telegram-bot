a3d827e9271033ab32d70305dc9bf6c50c6d32fa
sidor123 <74910028+sidor123@users.noreply.github.com>
2023-08-14 16:33:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -456,7 +456,7 @@ async def check_activity(message: types.Message) -> None:
         message_text += f"— <b>ID{info[0]}</b> <b>Дата</b>: " \
                         f"{datetime(info[3], info[2], info[1], int(info[4][:-2]), int(info[4][-2:]))} " \
                         f"<b>Группа: </b>{info[7]}\n"  # информация о занятии
-        undefined_students = await Students().check_a_by_b('name AND surname', 'group_id', info[7])  # берем имя и
+        undefined_students = await Students().check_a_by_b('name, surname', 'group_id', info[7])  # берем имя и
         # фамилию учеников из целой группы, закрепленной за занятием
         present_students_id = await Lessons().check_a_by_b('present_students', 'id', info[0])  # берем телеграм id
         # учеников, которые будут присутствовать
@@ -466,16 +466,16 @@ async def check_activity(message: types.Message) -> None:
         for student_id in present_students_id[0][0].split('/'):  # берем телеграм id из списка присутствующих
             if student_id:  # добавляем имя и фамилию учеников в созданный список
                 if present_students:
-                    present_students.append((await Students().check_a_by_b('name AND surname', 'telegram_id', student_id))[0])
+                    present_students.append((await Students().check_a_by_b('name, surname', 'telegram_id', student_id))[0])
                 else:
                     # тут создаётся список
-                    present_students = await Students().check_a_by_b('name AND surname', 'telegram_id', student_id)
+                    present_students = await Students().check_a_by_b('name, surname', 'telegram_id', student_id)
         for student_id in not_present_students_id[0][0].split('/'):  # тут берем из списка отсутствующих
             if student_id:
                 if not_present_students:
-                    not_present_students.append((await Students().check_a_by_b('name AND surname', 'telegram_id', student_id))[0])
+                    not_present_students.append((await Students().check_a_by_b('name, surname', 'telegram_id', student_id))[0])
                 else:
-                    not_present_students = await Students().check_a_by_b('name AND surname', 'telegram_id', student_id)
+                    not_present_students = await Students().check_a_by_b('name, surname', 'telegram_id', student_id)
         # ниже перебираем учеников из двух списков, что убрать лишние имена из списка "неопределенных"
         for n in range(len(present_students)):
             if present_students[n] in undefined_students:  # убираем присутствующих
